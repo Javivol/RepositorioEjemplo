bfd435c44b2786638342356ccb449c902727ecfa
TECH <TECH@CEP-AULA7-PC27>
2022-11-29 17:39:11 +0100

    a

--- src/otropaquete/HolaMundo.java
+++ src/otropaquete/HolaMundo.java
@@ -6,6 +6,6 @@ public class HolaMundo {
 		/*De esta forma se puede comentar
 		 * en más líneas
 		 */
-		System.out.println("adios mun+fghjfghjkdghdo");
+		System.out.println("adios mun+tyhrhrtjhghjfghjkdghdo");
 	}
 }
