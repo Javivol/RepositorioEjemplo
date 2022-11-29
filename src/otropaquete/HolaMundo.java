ab285769a2ff45f9ec36f6be6c2083110c4b675c
TECH <TECH@CEP-AULA7-PC27>
2022-11-29 17:31:34 +0100

    LJDAHFAS

---
+++ src/otropaquete/HolaMundo.java
@@ -0,0 +1,11 @@
+package otropaquete;
+
+public class HolaMundo {
+	public static void main(String[] args) {
+		//Este comando sirve para escribir en la consola
+		/*De esta forma se puede comentar
+		 * en más líneas
+		 */
+		System.out.println("adios mundo");
+	}
+}
